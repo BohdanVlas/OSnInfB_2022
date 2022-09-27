14422228a7fc4924b1749a4807189674dc0c0f62
BohdanVlas <112810591+BohdanVlas@users.noreply.github.com>
2022-09-27 21:36:11 +0300

    Add files via upload

---
+++ OsnInfB_PZ1/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Program
+{
+	static void rand(int a, int n)//Функція генерації послідовності псевдовипадкових чисел
+	{
+		Random rndm = new Random(n);//Початкове значення вводить користувач
+		for (int i = 0; i < a; i++)//цикл виведення послідовності з а псевдовипадкових чисел
+		{
+			Console.WriteLine(rndm.Next());
+		}
+	}
+	static void rand2(int a, int b, int c, int n)//Функція генерації послідовності псевдовипадкових чисел з границями введеними користувачем
+	{
+		Random rndm = new Random(n);
+		for (int i = 0; i < c; i++)//цикл виведення послідовності з а псевдовипадкових чисел
+		{
+			Console.WriteLine(rndm.Next(a, b));
+		}
+	}
+
+	public static byte[] randcr(int length)//Функція генерації криптографічно стійкого числа
+	{
+		using (var rndNumberGenerator = new RNGCryptoServiceProvider())
+		{
+			var randomNumber = new byte[length];
+			rndNumberGenerator.GetBytes(randomNumber);
+			return randomNumber;
+		}
+	}
+
+	public static void Main()
+	{
+		while (true)
+		{
+			Console.WriteLine("Generation of a sequence of pseudorandom numbers - 1\n" +//Меню
+							  "Generation of a sequence of pseudo-random numbers with variable limits - 2\n" +
+							  "Generation of a cryptographically stable sequence of random numbers - 3\n" +
+							  "Exit - 4\nChoose: ");
+			int A = Convert.ToInt32(Console.ReadLine());
+			if (A == 1)
+			{
+				Console.WriteLine("Enter the number of numbers in the sequence: ");
+				int B = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Enter seed: ");
+				int E = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("");
+				Program.rand(B, E);
+				Console.WriteLine("");
+			}
+			else if (A == 2)
+			{
+				Console.WriteLine("Enter the lower limit: ");
+				int C = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Enter the upper limit: ");
+				int B = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Enter seed: ");
+				int E = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Enter the number of numbers in the sequence: ");
+				int D = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("");
+				Program.rand2(C, B, D, E);
+				Console.WriteLine("");
+			}
+			else if (A == 3)
+			{
+				Console.WriteLine("Enter length: ");
+				int e = Convert.ToInt32(Console.ReadLine());
+				Console.WriteLine("Enter the number of numbers in the sequence: ");
+				int j = Convert.ToInt32(Console.ReadLine());
+				for (int i = 0; i < j; i++)
+				{
+					int rnd = BitConverter.ToInt32(randcr(e));
+					Console.WriteLine("");
+					Console.WriteLine(rnd);
+				}
+				Console.WriteLine("");
+			}
+			else if (A == 4)//Вихід
+			{
+				break;
+			}
+			else
+			{
+				continue;
+			}
+		}
+	}
+}
---
+++ OsnInfB_PZ2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+class Program
+{
+    static IEnumerable<string> GetAllPermutations(IEnumerable<char> chars, int n)
+    {
+        HashSet<char> curr = new HashSet<char>(chars);
+        foreach (var s in GetAllPermutationsRec(n, curr))
+            yield return s;
+    }
+    static IEnumerable<string> GetAllPermutationsRec(int n, HashSet<char> curr)
+    {
+        if (n == 0)
+            yield return "";
+        foreach (var c in curr.ToList())
+        {
+            curr.Remove(c);
+            foreach (var s in GetAllPermutationsRec(n - 1, curr))
+                yield return c + s;
+            curr.Add(c);
+        }
+    }
+    public static void Main()
+    {
+        var chars = new[] { 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '[', ']', '{', '}', ';', ':', '<', '>', ',', '.', '/', '?', '|', '`', '~', 'ё', 'Ё', '№', '*' };
+        while (true)
+        {
+            Console.WriteLine("Encrypting text from a file - 1\n" +
+                              "Decrypting text from a file - 2\n" +
+                              "Decrypting a file by the brute force method - 3\n" +
+                              "Exit - 4\nChoose: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            if (A == 1)
+            {
+                string read_data = File.ReadAllText("C:/Users/Bogdan/source/repos/OsnInfB_PZ2/test.txt");
+                byte[] readdata = Encoding.UTF8.GetBytes(read_data);
+                byte[] encdata = new byte[readdata.Length];
+                byte[] key = Encoding.UTF8.GetBytes("Password");
+                for (int i = 0; i < readdata.Length; i++)
+                {
+                    encdata[i] = (byte)(readdata[i] ^ key[i]);
+                };
+                File.WriteAllBytes("C:/Users/Bogdan/source/repos/OsnInfB_PZ2/test_1.dat", encdata);
+            }
+            else if (A == 2)
+            {
+                byte[] readdata = File.ReadAllBytes("C:/Users/Bogdan/source/repos/OsnInfB_PZ2/test_1.dat");
+                byte[] decdata = new byte[readdata.Length];
+                byte[] key = Encoding.UTF8.GetBytes("Password");
+                for (int i = 0; i < readdata.Length; i++)
+                {
+                    decdata[i] = (byte)(readdata[i] ^ key[i]);
+                };
+                string dec_data = Encoding.UTF8.GetString(decdata);
+                File.WriteAllText("C:/Users/Bogdan/source/repos/OsnInfB_PZ2/test_2.txt", dec_data);
+            }
+            else if (A == 3)
+            {
+                byte[] readdata = File.ReadAllBytes("C:/Users/Bogdan/source/repos/OsnInfB_PZ2/encfile5.dat");
+                var seq = GetAllPermutations(chars, 5);
+                foreach (var s in seq)
+                {
+                    byte[] key = new byte[5];
+                    key = Encoding.UTF8.GetBytes(s);
+                    Console.WriteLine(s);
+                    byte[] decdata = new byte[readdata.Length];
+                    int k = 0;
+                    for (int i = 0; i < readdata.Length; i++)
+                    {
+                        decdata[i] = (byte)(readdata[i] ^ key[k]);
+                        if (k == 4)
+                        {
+                            k = 0;
+                        }
+                        k++;
+                    };
+                    string dec_data = Encoding.UTF8.GetString(decdata);
+                    if (dec_data.Contains("Mit21"))
+                    {
+                        Console.WriteLine(dec_data);
+                        Console.WriteLine("If the text is legible and correct, enter 1, if not - any other number\n");
+                        int B = Convert.ToInt32(Console.ReadLine());
+                        if (B == 1)
+                        {
+                            File.WriteAllText("C:/Users/Bogdan/source/repos/OsnInfB_PZ2/decfile5.txt", dec_data);
+                            File.WriteAllBytes("C:/Users/Bogdan/source/repos/OsnInfB_PZ2/key.txt", key);
+                            break;
+                        }
+                        else
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
+            else if (A == 4)
+            {
+                break;
+            }
+            else
+            {
+                continue;
+            }
+        }
+    }
+}
